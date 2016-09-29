1c6c23d7015c0ebb2adc413c9c7bf6233430fe23
bernardo rodrigues <bernardoaraujor@gmail.com>
2016-09-29 14:14:13 -0300

    release 1.0

---
+++ wordlist scripts/usFirstNames.py
@@ -0,0 +1,52 @@
+import glob
+import operator
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+
+files = glob.glob("usFirstNames/*.txt")
+
+names = {}
+integral100 = 0
+
+for file in files:
+    f = open(file, 'r')
+    lines = f.readlines()
+    f.close()
+
+    for line in lines:
+        line = line.split(",")
+        name = line[0]
+        count = int(line[2].replace("\n", ""))
+
+        integral100 += count
+
+        if (name in names):
+            names[name] = names[name]+count
+        else:
+            names[name] = count
+
+names = OrderedDict(reversed(sorted(names.items(), key=operator.itemgetter(1))))
+
+integral = 0
+countList = []
+i = 0
+
+usNamesPopular = open('../src/org/owasp/passfault/wordlists/usFirstNamesPopular.words', 'w')
+usNamesLongTail = open('../src/org/owasp/passfault/wordlists/usFirstNamesLongTail.words', 'w')
+
+for name, count in names.items():
+    i += 1
+    integral += count
+    countList.insert(i, count)
+    if (integral <= 0.8*integral100):
+        usNamesPopular.write(name.lower() + '\n')
+    else:
+        usNamesLongTail.write(name.lower() + '\n')
+
+usNamesLongTail.close()
+usNamesPopular.close()
+
+# plt.plot(countList)
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.show()
